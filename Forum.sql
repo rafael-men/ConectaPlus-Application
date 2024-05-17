b6f481b26f8aa5a3325e29b81d8dae291f101bc7
HelenaCarvalhoLeal <hcleal19@gmail.com>
2024-05-17 00:04:54 -0300

    forum
    ajuste no forum.sql

--- Forum.sql
+++ Forum.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `forum` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `forum`;
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: forum
@@ -91,6 +93,14 @@ LOCK TABLES `tb_pessoa_area` WRITE;
 INSERT INTO `tb_pessoa_area` VALUES (17,4),(18,2),(19,3),(20,1);
 /*!40000 ALTER TABLE `tb_pessoa_area` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping events for database 'forum'
+--
+
+--
+-- Dumping routines for database 'forum'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -101,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-11 21:32:39
+-- Dump completed on 2024-05-16 23:42:59
